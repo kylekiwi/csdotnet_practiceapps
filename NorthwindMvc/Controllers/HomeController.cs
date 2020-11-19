d1f843d96a43ead1fc28fdc1a1d4d6278c787680
Kyle <hijongwon@gmail.com>
2020-11-20 07:57:24 +0900

    View Page Add

--- NorthwindMvc/Controllers/HomeController.cs
+++ NorthwindMvc/Controllers/HomeController.cs
@@ -6,23 +6,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NorthwindMvc.Models;
+using Packt.Shared;
 
 namespace NorthwindMvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private Northwind db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, Northwind injectedContext)
         {
             _logger = logger;
+            db = injectedContext;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var model = new HomeIndexViewModel
+            {
+                VisitorCount    = (new Random()).Next(1, 1001),
+                Categories      = db.Categories.ToList(),
+                Products        = db.Products.ToList()
+            };
+            return View(model);
         }
 
+        // [Route("private")]
         public IActionResult Privacy()
         {
             return View();
